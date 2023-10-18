7809c6251cd16e2fecc5e1e257ddd3dcaacaae40
isaac <isaacmuchunu@gmail.com>
2023-10-18 18:59:37 +0300

    Made some changes to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  const year = new Date().getFullYear(); 
+  const year = new Date().getFullYear();
 
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <div>
-          <span className="font-bold text-xl">Logo</span> 
-          <p className="text-sm">CyberShield</p> 
+          <span className="font-bold text-xl">Logo</span>
         </div>
         <div>
-          <p className="text-slate-600">All rights reserved © {year}.</p> 
+          <p className="text-slate-600">All rights reserved © {year}.</p>
         </div>
       </div>
     </footer>
